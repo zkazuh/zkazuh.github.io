8b9d72d6e25ca3bbd46938167517bd1927e94275
Danilo Cercal Lara <danilocercal@proton.me>
2025-12-06 12:13:29 -0300

    adicionado alguns jogos novos

--- js/jogos.js
+++ js/jogos.js
@@ -3,27 +3,130 @@ window.catalogoJogos = {
         {
             nome: "Spider-Man 2",
             genero: "Ação",
-            capa: "img/spiderman2.avif",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202306/1219/97e9f5fa6e50c185d249956c6f198a2652a9217e69a59ecd.jpg",
             descricao: "Peter e Miles enfrentam Venom em NYC."
         },
         {
             nome: "God of War Ragnarök",
             genero: "Ação/Aventura",
-            capa: "img/gow_ragnarok.jpg",
+            capa: "https://gmedia.playstation.com/is/image/SIEPDC/god-of-war-raganok-clean-keyart-01-en-10mar22?$4000px$",
             descricao: "Kratos enfrenta o fim dos tempos."
+        },
+        {
+            nome: "Ratchet & Clank: Em Uma Outra Dimensão",
+            genero: "Ação / Plataforma",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202101/2921/qmTbXLhip3iLXIoxhh4H6Ldu.jpg",
+            descricao: "Pule de dimensão em dimensão com Ratchet e Clank para enfrentar um imperador do mal de outra realidade."
+        },
+        {
+            nome: "Returnal",
+            genero: "Roguelike / Tiro",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202011/1621/oE5NHcrITZnKa1kYSCQtsoRE.png",
+            descricao: "Após cair em um mundo alienígena que muda de forma, Selene deve lutar com unhas e dentes para sobreviver e quebrar o ciclo."
+        },
+        {
+            nome: "Demon's Souls",
+            genero: "RPG de Ação",
+            capa: "https://image.api.playstation.com/vulcan/img/rnd/202011/1717/brIXKBE5BqYgBSrsDn6Wo18O.png",
+            descricao: "Remake completo do clássico da PlayStation, onde você enfrenta desafios brutais e chefes impiedosos no reino de Boletaria."
+        },
+        {
+            nome: "Death Stranding Director's Cut",
+            genero: "Ação / Mundo Aberto",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202106/2214/Lm3hCOYIsnXvq0GbQkBVcRvD.jpg",
+            descricao: "Sam Bridges deve desbravar um mundo transformado pelo Death Stranding, carregando os restos desconectados do nosso futuro."
+        },
+        {
+            nome: "Final Fantasy VII Remake Intergrade",
+            genero: "RPG",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202102/1515/NF3gnTCeqXS2yqehtTQBYkOO.jpg?",
+            descricao: "Uma reimaginação espetacular de um dos jogos mais visionários de todos os tempos, otimizado para o PS5."
+        },
+        {
+            nome: "Ghost of Tsushima: Director's Cut",
+            genero: "Ação / Aventura",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202106/2322/FEhFbXbOMnlh1y8Mi9dPuJHQ.png",
+            descricao: "Explore a ilha de Tsushima em 4K e 60fps, lutando como o Fantasma para libertar sua terra natal da invasão mongol."
         }
-    ],
+        ],
 
     xbox: [
         {
             nome: "Forza Horizon 5",
             genero: "Corrida",
-            capa: "img/forza5.avif",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202501/2717/0c5df2b67b23263d055f3b78aeb77a6ce4668bb078fced77.jpg",
             descricao: "Dirija pelo México em mundo aberto."
+        },
+
+        {
+            nome: "Halo Infinite",
+            genero: "FPS / Tiro",
+            capa: "https://wpassets.halowaypoint.com/wp-content/2021/12/HaloInfinite_CampaignKeyArt_CLEAN_1920x1080.jpg",
+            descricao: "O Master Chief retorna para enfrentar o inimigo mais implacável que já encontrou, explorando a vastidão do anel Halo."
+        },
+        {
+            nome: "Starfield",
+            genero: "RPG de Ação",
+            capa: "https://cdn.wccftech.com/wp-content/uploads/2021/06/Starfield_613_HERO.jpg",
+            descricao: "O primeiro novo universo em 25 anos da Bethesda Game Studios. Crie qualquer personagem que quiser e explore com liberdade inigualável."
+        },
+        {
+            nome: "Senua's Saga: Hellblade II",
+            genero: "Ação / Aventura Narrativa",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202505/2009/ff0306069a922b65c257e00c0392816b94002dae861dd0cf.png",
+            descricao: "Na sequência do premiado Hellblade, Senua retorna em uma jornada brutal de sobrevivência através do mito e tormento na Islândia viking."
+        },
+        {
+            nome: "Hi-Fi RUSH",
+            genero: "Ação / Ritmo",
+            capa: "https://www.geekinout.pt/storage/265/01HT85HQ5AMT9FZABQXW16Q9MK.png",
+            descricao: "Sinta a batida enquanto o aspirante a astro do rock Chai e sua equipe desorganizada enfrentam uma megacorporação maligna de aprimoramentos robóticos."
+        },
+        {
+            nome: "Gears 5",
+            genero: "Tiro em Terceira Pessoa",
+            capa: "https://static0.srcdn.com/wordpress/wp-content/uploads/2019/09/Gears-5-Review.jpg?w=1600&h=900&fit=crop",
+            descricao: "Com a guerra total descendo, Kait Diaz se separa para descobrir sua conexão com o inimigo e descobre o verdadeiro perigo para Sera: ela mesma."
         }
     ],
 
     switch: [
+        {
+            nome: "The Legend of Zelda: Ocarina of Time",
+            genero: "Ação / Aventura",
+            capa: "",
+            descricao: "Acompanhe Link em uma jornada lendária através do tempo para impedir Ganondorf de obter a Triforce, no clássico que definiu os jogos 3D."
+        },
+        {
+            nome: "Super Mario 64",
+            genero: "Plataforma 3D",
+            capa: "",
+            descricao: "O jogo que revolucionou o gênero de plataforma. Explore o castelo da Princesa Peach e pule dentro de pinturas mágicas para coletar Estrelas."
+        },
+        {
+            nome: "Metroid Fusion",
+            genero: "Ação / Metroidvania",
+            capa: "",
+            descricao: "Samus Aran é infectada por um parasita misterioso e deve lutar contra uma cópia mortal de si mesma, a SA-X, em uma estação espacial abandonada."
+        },
+        {
+            nome: "Mario Kart 64",
+            genero: "Corrida",
+            capa: "",
+            descricao: "Corra com Mario e seus amigos em pistas icônicas neste clássico multiplayer, usando cascos e bananas para garantir a vitória."
+        },
+        {
+            nome: "F-Zero 99",
+            genero: "Corrida / Battle Royale",
+            capa: "",
+            descricao: "Um jogo exclusivo para assinantes onde 99 pilotos competem simultaneamente em pistas clássicas de alta velocidade."
+        },
+        {
+            nome: "The Legend of Zelda: The Minish Cap",
+            genero: "Ação / Aventura",
+            capa: "",
+            descricao: "Link descobre um chapéu falante que o permite encolher ao tamanho de um inseto, explorando Hyrule de uma perspectiva totalmente nova."
+        },
         {
             nome: "Zelda Breath of the Wild",
             genero: "Aventura",
@@ -34,9 +137,45 @@ window.catalogoJogos = {
 
     ps4: [
         {
+            nome: "God of War",
+            genero: "Ação / Aventura",
+            capa: "https://image.api.playstation.com/vulcan/img/rnd/202010/2217/KAmUQWQ5V9QF3XDzmty1VkKj.png",
+            descricao: "Kratos vive agora como um mortal no reino dos deuses e monstros nórdicos. É nesse mundo duro e implacável que ele deve lutar para sobreviver... e ensinar seu filho a fazer o mesmo."
+        },
+        {
+            nome: "Bloodborne",
+            genero: "RPG de Ação",
+            capa: "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/O2Z66UWrZH8zcejxopwWxhGu.png",
+            descricao: "Enfrente seus pesadelos enquanto busca respostas na antiga cidade de Yharnam, agora amaldiçoada com uma estranha doença endêmica que se espalha pelas ruas como fogo."
+        },
+        {
+            nome: "Ghost of Tsushima: Director's Cut",
+            genero: "Ação / Aventura",
+            capa: "https://image.api.playstation.com/vulcan/ap/rnd/202106/2322/FEhFbXbOMnlh1y8Mi9dPuJHQ.png",
+            descricao: "Na Tsushima do final do século XIII, o guerreiro samurai Jin Sakai é um dos últimos sobreviventes de seu clã e deve fazer o que for preciso para proteger seu povo e recuperar seu lar."
+        },
+        {
+            nome: "Marvel's Spider-Man: Miles Morales",
+            genero: "Ação / Super-herói",
+            capa: "https://image.api.playstation.com/vulcan/img/rnd/202010/2417/tnCutdREPv6Pa7atqb8MTxGW.png",
+            descricao: "Acompanhe a trajetória de Miles Morales enquanto ele aprende a dominar poderes incríveis e explosivos para se tornar o novo Spider-Man."
+        },
+        {
+            nome: "Horizon Zero Dawn: Complete Edition",
+            genero: "RPG de Ação",
+            capa: "https://image.api.playstation.com/vulcan/img/rnd/202009/2923/LyLrdlIfqiVzvynWJGtNfbU8.png",
+            descricao: "Assuma o papel da habilidosa caçadora Aloy enquanto explora um mundo vibrante e exuberante habitado por misteriosas criaturas mecanizadas."
+        },
+        {
+            nome: "Demon's Souls",
+            genero: "RPG de Ação",
+            capa: "https://image.api.playstation.com/vulcan/img/rnd/202011/1717/brIXKBE5BqYgBSrsDn6Wo18O.png",
+            descricao: "Descubra onde a jornada começou. Experimente o desafio brutal original, completamente refeito do zero."
+        },
+        {
             nome: "The Last of Us Part II",
             genero: "Ação",
-            capa: "img/tlou2.jpg",
+            capa: "https://image.api.playstation.com/vulcan/img/rnd/202010/2618/UIRjzDjQKrOD4kx6ubmlOsrA.png?",
             descricao: "Ellie busca respostas."
         }
     ]
